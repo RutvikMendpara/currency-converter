c8948cf712b1718f483c3f7cd018cb747300a9a2
RutvikMendpara <rutvikm200@gmail.com>
2022-07-06 17:31:40 +0530

    final-upload

---
+++ app.js
@@ -0,0 +1,74 @@
+const cur1 = document.getElementById("cur1");
+const cur2 = document.getElementById("cur2");
+const submit = document.getElementById("submit");
+const showRate = document.getElementById("showRate");
+
+// show currency
+const AddCurrency = () => {
+  async function getData() {
+    const response = await fetch(`https://api.exchangerate.host/symbols`);
+    const curlist = await response.json();
+
+    const currencyList = Object.values(curlist.symbols);
+
+    for (const x of currencyList) {
+      let option1 = document.createElement("option");
+      let option2 = document.createElement("option");
+      option1.innerHTML = `${x.code} - ${x.description}`;
+      option1.setAttribute("value", `${x.code}`);
+      option2.innerHTML = `${x.code} - ${x.description}`;
+      option2.setAttribute("value", `${x.code}`);
+      cur1.appendChild(option1);
+      cur2.appendChild(option2);
+      // console.log(x);
+    }
+
+    return curlist;
+  }
+  getData().catch((err) => console.log("errors:", err.message));
+};
+
+//clear screen
+const clearScreen = () => {
+  while (showRate.hasChildNodes()) {
+    showRate.removeChild(showRate.firstChild);
+  }
+};
+
+// find rate
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const showCountry = document.createElement("div");
+  const curFrom = document.createElement("div");
+  const curTo = document.createElement("div");
+  const curRate = document.createElement("div");
+  clearScreen(curFrom, curTo, curRate);
+
+  // append elements
+  showRate.appendChild(showCountry);
+  showRate.appendChild(curFrom);
+  showRate.appendChild(curTo);
+  showRate.appendChild(curRate);
+
+  async function getData() {
+    const response = await fetch(
+      `https://api.exchangerate.host/convert?from=${cur1.value}&to=${cur2.value}`
+    );
+    const data = await response.json();
+    showCountry.classList.add("h1");
+    showCountry.classList.add("text-warning");
+    showCountry.innerHTML = `Rate of ${JSON.stringify(
+      data.query.from
+    )} to ${JSON.stringify(data.query.to)} is ${JSON.stringify(data.result)}`;
+
+    return data;
+  }
+  getData()
+    // .then((data) => console.log(data))
+    .catch((err) => console.log("errors:", err.message));
+});
+
+// load
+
+AddCurrency();
